cf06d5c63915e765aac07d05b6b485cc57e7fcf6
Neale Swinnerton <neale@isismanor.com>
2015-01-08 17:13:56 +0000

    Update aws sdk version.

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [com.amazonaws/aws-java-sdk "1.7.5"]
+                 [com.amazonaws/aws-java-sdk "1.9.13"]
                  [clj-time "0.6.0"]]
   :plugins [[codox "0.8.10"]])
